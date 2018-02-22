af53899f975a6260e80209c7e55dd2a1b69a498b
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-22 00:09:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ class App < Sinatra::Base
     @word2 = params[:word2]
     @word3 = params[:word3]
     @word4 = params[:word4]
-    "#{@word1} #{@word1} #{@word1} #{@word1}."
+    "#{@word1} #{@word2} #{@word3} #{@word4}."
   end
 
   get '/:operation/:number1/:number2' do
